7ca56c83d9dd1967a87dbcf6cb06855dfe98f284
vcorpsys <vcorpsys@gmail.com>
2019-08-02 02:52:20 -0300

    atualizacao_03

--- www/js/lista.js
+++ www/js/lista.js
@@ -101,8 +101,8 @@ function selecionar() {
 		"<tr>" +
 			"<td><label class='switch'><input type='checkbox' id='check_" + i + "' onchange='marcar(" + i + ")' " + str + "><span class='slider round'></span></label></td>" +
 			"<td id='nome_" + i + "' style='word-wrap: break-word; word-break: break-all;'>" + item.Nome + "</td>" +
-			"<td align='right'><button type='button' class='btn btn-primary' onclick='selecionarUm(" + i + ")' data-toggle='modal' data-target='#modalEditar'>e</button>" +
-			"<button type='button' class='btn btn-danger' data-toggle='modal' onclick='selecionarDel(" + i + ")' data-target='#modalDeletar'>x</button></td>" +
+			"<td align='right'><button type='button' class='btn btn-sm btn-primary' onclick='selecionarUm(" + i + ")' data-toggle='modal' data-target='#modalEditar'>e</button>" +
+			"<button type='button' class='btn btn-sm btn-danger' data-toggle='modal' onclick='selecionarDel(" + i + ")' data-target='#modalDeletar'>x</button></td>" +
 		"</tr>";
 
 		opcoes += "<option value='" + item.Nome + "'>" + item.Nome + "</option>";
